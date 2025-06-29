237d7fed2e4d306c1078635135735ec04c95213a
ttanuj <ttanujreddy@gmail.com>
2025-06-29 14:52:18 -0700

    Initialize board memory and render board.

--- Tic-Tac-Toe.asm
+++ Tic-Tac-Toe.asm
@@ -16,7 +16,70 @@
 .text
 .globl main
 	main:
-    	# TODO: initialize board
-    	# TODO: game loop
-	    li $v0, 10
+	# Initialize board to zeros
+    la $t0, board
+    li $t1, 9
+
+	init_loop:
+    	beq $t1, $zero, main_loop
+  		sw $zero, 0($t0)
+    	addi $t0, $t0, 4
+    	addi $t1, $t1, -1
+    	j init_loop
+
+	main_loop:
+    	# Render the board
+    	jal drawBoard
+    	li $v0, 10
     	syscall
+    
+    # drawBoard: print 3×3 grid
+	drawBoard:
+    	addi $sp, $sp, -8
+    	sw $ra, 4($sp)
+    	la $t0, board
+    	li $t1, 0            # row index
+
+	draw_row:
+    	li $t2, 0            # col index
+
+	draw_col:
+    	mul $t3, $t1, 3
+    	add $t3, $t3, $t2
+	    sll $t3, $t3, 2
+    	add $t3, $t3, $t0
+    	lw $t4, 0($t3)
+	    beq $t4, $zero, printE
+    	li $t5, 1
+	    beq $t4, $t5, printX
+    	j printO
+
+	printE:
+	    la $a0, charE
+    	li $v0, 4
+	    syscall
+    	j cont
+
+	printX:
+	    la $a0, charX
+    	li $v0, 4
+	    syscall
+    	j cont
+
+	printO:
+    	la $a0, charO
+	    li $v0, 4
+    	syscall
+
+	cont:
+	    addi $t2, $t2, 1
+    	blt $t2, 3, draw_col
+	    la $a0, newline
+    	li $v0, 4
+	    syscall
+    	addi $t1, $t1, 1
+	    blt $t1, 3, draw_row
+
+	    lw $ra, 4($sp)
+    	addi $sp, $sp, 8
+	    jr $ra
